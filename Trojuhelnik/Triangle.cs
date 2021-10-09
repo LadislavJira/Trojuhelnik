4aaba2c50575f1e9e826286842a1f8f77b1cbd01
LadislavJira <ladislav.jira.020@pslib.cz>
2021-10-09 11:24:21 +0200

    vsechny metody

--- Trojuhelnik/Triangle.cs
+++ Trojuhelnik/Triangle.cs
@@ -8,70 +8,91 @@ namespace Trojuhelnik
 {
     class Triangle
     {
-        private float _a;
-        private float _b;
-        private float _c;
+        private double _a;
+        private double _b;
+        private double _c;
 
-        public Triangle(float a, float b, float c)
+        public Triangle(double a, double b, double c)
         {
-            _a = a;
-            _b = b;
-            _c = c;
+        if (a > 0)
+            {
+                _a = a;
+            }
+        if (b > 0)
+            {
+                _b = b;
+            }
+        if (c > 0)
+            {
+                _c = c;
+            }
         }
 
-        public Triangle(float a)
+        public Triangle(double a)
         {
+            if (a > 0)
+            {
             _a = a;
             _b = a;
             _c = a;
+            }
         }
 
-        public void SetA(float value)
+        public void SetA(double value)
         {
             _a = value;
         }
 
-        public float GetA()
+        public double GetA()
         {
             return _a;
         }
 
-        public void SetB(float value)
+        public void SetB(double value)
         {
             _b = value;
         }
 
-        public float GetB()
+        public double GetB()
         {
             return _b;
         }
 
-        public void SetC(float value)
+        public void SetC(double value)
         {
             _c = value;
         }
 
-        public float GetC()
+        public double GetC()
         {
             return _c;
         }
 
-        public void SetSides(float valueA, float valueB, float valueC)
+        public void SetSides(double valueA, double valueB, double valueC)
         {
-            _a = valueA;
-            _b = valueB;
-            _c = valueC;
+            if (valueA > 0)
+            {
+                _a = valueA;
+            }
+            if (valueB > 0)
+            {
+                _b = valueB;
+            }
+            if (valueC > 0)
+            {
+                _c = valueC;
+            }
         }
 
-        public float GetArea()
+        public double GetArea()
         {
-            float s = (_a + _b + _c) / 2;
-            float area = (float)Math.Sqrt(s * (s - _a) * (s - _b) * (s - _c));
+            double s = (_a + _b + _c) / 2;
+            double area = Math.Sqrt(s * (s - _a) * (s - _b) * (s - _c));
             return area;
         }
-         public float GetOutline()
+         public double GetOutline()
         {
-            float outline = _a + _b + _c;
+            double outline = _a + _b + _c;
             return outline;
         }
         public bool IsExisting()
@@ -84,6 +105,38 @@ namespace Trojuhelnik
             {
                 return false;
             }
+        }
+        public static bool CanExist(double a,  double b,  double c)
+        {
+            if ((a + b > c) && (a + b > c) && (b + c > a))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double GetAlpha()
+        {
+            double s = (_a + _b + _c) / 2;
+            double alpha = Math.Asin(2 * Math.Sqrt(s * (s - _a) * (s - _b) * (s - _c)) / (_b * _c));
+            alpha = (180 / Math.PI) * alpha;
+            return alpha;
+        }
+        public double GetBeta()
+        {
+            double s = (_a + _b + _c) / 2;
+            double beta = Math.Asin(2 * Math.Sqrt(s * (s - _a) * (s - _b) * (s - _c)) / (_a * _c));
+            beta = (180 / Math.PI) * beta;
+            return beta;
+        }
+        public double GetGamma()
+        {
+            double s = (_a + _b + _c) / 2;
+            double gamma = Math.Asin(2 * Math.Sqrt(s * (s - _a) * (s - _b) * (s - _c)) / (_a * _b));
+            gamma = (180 / Math.PI) * gamma;
+            return gamma;
         }
     }
 }
